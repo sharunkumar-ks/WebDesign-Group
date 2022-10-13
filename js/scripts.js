fb61edef9af95b5437dd2797aa2f2bb9902cecd4
Sharun <sharunksplus@gmail.com>
2022-10-13 12:42:08 -0400

    added toast notification for e-mail subscription

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,12 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+var toastList = toastElList.map(function (toastEl) {
+    return new bootstrap.Toast(toastEl, {})
+})
+
+function showToast() {
+    toastList[0].show()
+}
