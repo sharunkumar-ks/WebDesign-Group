8659cf4b6fee1fd376afde26d656d174fd447264
Sharun Kumar Kakkad Sasikumar <kakkadsasikumar.s@northeastern.edu>
2022-12-03 16:46:59 -0500

    added route for space

---
+++ webdesign-project-frontend/src/pages/space/[id].tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+
+const OfficeSpace: NextPage = () => {
+
+    const router = useRouter();
+
+    const { id } = router.query;
+    // TODO: check if the ID exists in the database, else return an element that says ID does not exist
+
+    return <>
+        <h1>Office Space of ID {id}</h1>
+    </>
+}
+
+export default OfficeSpace
