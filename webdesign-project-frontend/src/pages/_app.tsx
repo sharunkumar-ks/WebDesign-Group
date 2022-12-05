e43865bb6c37ca698858a62e21a1388334bdccff
Sharun Kumar Kakkad Sasikumar <kakkadsasikumar.s@northeastern.edu>
2022-12-04 23:37:59 -0500

    imported globals.css

--- webdesign-project-frontend/src/pages/_app.tsx
+++ webdesign-project-frontend/src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { SessionProvider } from "next-auth/react";
 
 import { trpc } from "../utils/trpc";
 
-// import "../styles/globals.css";
+import "../styles/globals.css";
 
 import 'bootstrap/dist/css/bootstrap.css';
 import Navbar from "../components/NavBar";
